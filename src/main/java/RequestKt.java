e0fa1828d193a3c6621722b575124f649936ff8e
Michael <michael.voser@confinale.ch>
2020-10-22 17:01:06 +0200

    optimize imports

---
+++ src/main/java/RequestKt.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.ProxySelector;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class RequestKt {
+  public static void main(String[] args) throws IOException, InterruptedException {
+
+  DefaultApiKt.main(new String[]{});
+
+  }
+}
