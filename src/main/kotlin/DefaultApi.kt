f5d09f30cf7339012b5d4446fa8be8f564f8a324
Michael <michael.voser@confinale.ch>
2020-10-22 14:08:22 +0200

    optimize imports

--- src/main/kotlin/DefaultApi.kt
+++ src/main/kotlin/DefaultApi.kt
@@ -6,30 +6,34 @@ import io.ktor.client.statement.HttpResponse
 import io.ktor.util.cio.toByteArray
 import io.ktor.utils.io.core.String
 import kotlinx.coroutines.runBlocking
+import io.ktor.client.engine.okhttp.*
 
 fun main(args: Array<String>) {
     runBlocking {
-        val client = HttpClient() {
+
+        val client = HttpClient(OkHttp) {
             followRedirects = true
         }
 
-        val response: HttpResponse = client.put<HttpResponse> {
-            url("https://vstde-t.estv.admin.ch/api/md/m2m/register/bo")
-            header("Authorization", System.getenv().get("token"))
-            body = "{\n" +
-                    "   \"person\":{\n" +
-                    "      \"type\":\"natural\",\n" +
-                    "      \"firstName\":\"Christian\",\n" +
-                    "      \"lastName\":\"Brüesch\",\n" +
-                    "      \"birthDate\":\"1980-03-09\"\n" +
-                    "   },\n" +
-                    "   \"zip\":\"10115\",\n" +
-                    "   \"city\":\"Berlin\",\n" +
-                    "   \"state\":\"DE\",\n" +
-                    "   \"taxId\":{\n" +
-                    "      \"taxNumber\":\"013/345/67891\"\n" +
-                    "   }\n" +
-                    "}"
+        val response: HttpResponse = client.use { client ->
+            client.put<HttpResponse> {
+                url("https://vstde-t.estv.admin.ch/api/md/m2m/register/bo")
+                header("Authorization", System.getenv().get("token"))
+                body = "{\n" +
+                        "   \"person\":{\n" +
+                        "      \"type\":\"natural\",\n" +
+                        "      \"firstName\":\"Hans\",\n" +
+                        "      \"lastName\":\"Müller\",\n" +
+                        "      \"birthDate\":\"1980-03-09\"\n" +
+                        "   },\n" +
+                        "   \"zip\":\"10115\",\n" +
+                        "   \"city\":\"Berlin\",\n" +
+                        "   \"state\":\"DE\",\n" +
+                        "   \"taxId\":{\n" +
+                        "      \"taxNumber\":\"013/345/67891\"\n" +
+                        "   }\n" +
+                        "}"
+            }
         }
         println(String(response.content.toByteArray()))
     }
