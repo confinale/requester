eea2a70c5413197d7bee80cd3e5a8fa275d971b2
Michael <michael.voser@confinale.ch>
2020-10-28 17:23:35 +0100

    1.2

--- src/main/kotlin/DefaultApiAT.kt
+++ src/main/kotlin/DefaultApiAT.kt
@@ -1,16 +1,29 @@
 import io.ktor.client.HttpClient
 import io.ktor.client.engine.ProxyBuilder
 import io.ktor.client.engine.http
+import io.ktor.client.request.get
 import io.ktor.client.statement.HttpResponse
 import io.ktor.util.cio.toByteArray
 import io.ktor.utils.io.core.String
 import kotlinx.coroutines.runBlocking
-import io.ktor.client.engine.okhttp.*
-import io.ktor.client.request.get
 
 fun main(args: Array<String>) {
     runBlocking {
-        val httpProxy = ProxyBuilder.http(args[0])
+
+        val proxyHost = System.getProperty("https.proxyHost")
+        println("proxyHost: "+proxyHost)
+        val proxyPort = System.getProperty("https.proxyPort")
+        println("proxyPort: "+proxyPort)
+        var url = ""
+
+        if (args.size > 1) {
+            url=args[1]
+            println("proxyConfig from arguments.")
+        }else{
+            url="https://"+proxyHost+":"+proxyPort
+        }
+
+        val httpProxy = ProxyBuilder.http(url)
         val client = HttpClient() {
             followRedirects = true
             engine {
@@ -18,15 +31,15 @@ fun main(args: Array<String>) {
             }
         }
         try {
-                val response: HttpResponse = client.get(args[1])
-                //val response: HttpResponse = client.get("https://www.formularservice.gv.at/site/fsrv/user/formular.aspx?pid=09e05dcbe1f84f51afb7b482d272accb&pn=B9d218f001088468a9727ec6fffd85b1b")
-                //println(String(response.content.toByteArray()))
-                println("------------- response.status -------------")
-                println(response.status)
-                println("------------- response.headers -------------")
-                response.headers.forEach { s, list -> println(s + ": " + list) }
-                println("------------- response.content -------------")
-                println(String(response.content.toByteArray()))
+            val response: HttpResponse = client.get(args[0])
+            //val response: HttpResponse = client.get("https://www.formularservice.gv.at/site/fsrv/user/formular.aspx?pid=09e05dcbe1f84f51afb7b482d272accb&pn=B9d218f001088468a9727ec6fffd85b1b")
+            //println(String(response.content.toByteArray()))
+            println("------------- response.status -------------")
+            println(response.status)
+            println("------------- response.headers -------------")
+            response.headers.forEach { s, list -> println(s + ": " + list) }
+            println("------------- response.content -------------")
+            println(String(response.content.toByteArray()))
 
         } catch (e: Exception) {
             println(e.message)
